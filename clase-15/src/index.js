9193dfe7b9ece87bba15c86fe3aeb7d7aab14ac8
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-12 14:41:17 -0300

    add: basic styling to the page

--- clase-15/src/index.js
+++ clase-15/src/index.js
@@ -1,3 +1,20 @@
-const world = 'world'
+const $btnAdd = document.querySelector('#btnAdd')
 
-console.log(world)
+$btnAdd.addEventListener('click', () => {
+  const inputValue = getInputValue()
+  const item = createItem(inputValue)
+  const $list = document.querySelector('#itemsList')
+  $list.appendChild(item)
+})
+
+function getInputValue() {
+  const value = document.querySelector('.input-container input').value
+  return value
+}
+
+function createItem(value) {
+  const $item = document.createElement('li')
+  $item.className = 'list__item'
+  $item.innerText = value
+  return $item
+}
