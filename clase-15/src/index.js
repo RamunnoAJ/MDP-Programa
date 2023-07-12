3224e5b63a376d8b87730bd5855adbb7d4bfb426
RamunnoAJ <ramunnoagustin@gmail.com>
2023-07-12 17:38:05 -0300

    add: functionality to add and remove tasks

--- clase-15/src/index.js
+++ clase-15/src/index.js
@@ -1,20 +1,34 @@
-const $btnAdd = document.querySelector('#btnAdd')
+const $form = document.querySelector('#form')
 
-$btnAdd.addEventListener('click', () => {
-  const inputValue = getInputValue()
-  const item = createItem(inputValue)
+$form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const $input = document.querySelector('.input-container input')
+  const value = $input.value
+  const item = createItem(value)
+  $input.value = ''
   const $list = document.querySelector('#itemsList')
   $list.appendChild(item)
 })
 
-function getInputValue() {
-  const value = document.querySelector('.input-container input').value
-  return value
-}
-
 function createItem(value) {
   const $item = document.createElement('li')
   $item.className = 'list__item'
-  $item.innerText = value
+  const $itemText = document.createElement('span')
+  $itemText.innerText = value
+
+  $itemText.addEventListener('click', () => {
+    $itemText.classList.toggle('marked')
+  })
+
+  const $removeBtn = document.createElement('button')
+  $removeBtn.className = 'btn-remove'
+  $removeBtn.innerText = 'X'
+  $removeBtn.addEventListener('click', () => {
+    $item.remove()
+  })
+
+  $item.appendChild($itemText)
+  $item.appendChild($removeBtn)
+
   return $item
 }
